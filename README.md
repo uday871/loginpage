25515b7947df46e01937e2e112e21f65cfeebc21
uday871 <uday871@gmail.com>
2024-08-07 15:37:06 +0530

    this is my new login

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Getting Started with Create React App
+git push -u origin maingit add loginpage# Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
---
+++ src/Register.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('http://localhost:8000/api/auth/register', { username, password });
+      alert('Registration successful');
+    } catch (err) {
+      alert('Registration failed');
+    }
+  };
+
+  return (
+    <div className="register-container">
+      <h2>Register</h2>
+      <form onSubmit={handleRegister}>
+        <div>
+          <label>Username:</label>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
